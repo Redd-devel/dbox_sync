f21c856fffeda29bda61f06ffac84768fa628393
Redd-devel <ms_redgard@hotmail.com>
2021-09-20 19:52:39 +0600

    added new function: last_files_finder

--- dbox_sync_lib.py
+++ dbox_sync_lib.py
@@ -2,8 +2,10 @@ import dropbox
 import os
 import sys
 import glob
+import datetime
 
 from dropbox.exceptions import ApiError, AuthError
+from dbox_config import DBOX_FOLDER
 
 def instantiate_dropbox():
     """ Make Dropbox instance"""
@@ -16,6 +18,7 @@ def instantiate_dropbox():
     try:
         dbx.users_get_current_account()
     except AuthError as err:
+        print(err)
         sys.exit(
             "ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
     return dbx
@@ -29,6 +32,22 @@ def clean_dbox_folder(dbox_dir):
         except ApiError as err:
             print(f'Something wrong with {file.path_display}. Reason: {err}')
 
-def collect_files(filemask: str) -> list():
+def collect_files(filemask: str) -> list:
     """Collect files by a mask"""
     return glob.glob(filemask)
+
+def last_files_finder():
+    """Find last actual file"""
+    dbx = instantiate_dropbox()
+    days=0
+    while days < 182:
+        date_mask = (datetime.date.today() - \
+            datetime.timedelta(days=days)).strftime("%Y-%m-%d")
+        filemask = "projects_" + date_mask + ".zip.asc"
+        if dbx.files_search(DBOX_FOLDER, filemask).matches:
+            return dbx.files_search(DBOX_FOLDER, filemask).matches[0].metadata.path_display
+        days += 1
+    return
+
+if __name__ == '__main__':
+    print(last_files_finder())
