bcb39d39760caf4c6f287eb38f04a8f3dd4277ba
Redd-devel <ms_redgard@hotmail.com>
2021-10-02 07:16:35 +0600

    Refactored upload function

--- shared_libs/dbox_sync_lib.py
+++ shared_libs/dbox_sync_lib.py
@@ -2,7 +2,6 @@ import dropbox
 import os
 from pathlib import Path
 import sys
-import glob
 import datetime
 import shutil
 from sh import rsync, gpg
@@ -12,7 +11,62 @@ from shared_libs.dbox_config import WORK_DIR, SOURCE_ITEMS, \
                 GPG_ID, CURRENT_DATE, RETENTION_PEROD
 
 
-def upload_file(dbox_instance, dest_folder, dest_file):
+def upload():
+    """Copy files, folders to destination"""
+    source_list = SOURCE_ITEMS
+    dbx = instantiate_dropbox()
+    remove_old_files(WORK_DIR)
+    relative_dirs = list(source_list.keys())
+    for folder in relative_dirs:
+        full_backup_dir = Path(WORK_DIR).joinpath(folder).__str__()
+        for source_dir in source_list[folder]:
+            rsync("-avrh", "--exclude=*.pyc", "--exclude=*.log*", "--exclude=.vscode", "--delete", source_dir, full_backup_dir) # "-R"
+        # rsync("-avrh", "--exclude=*.pyc", "--exclude=*.log*", "--exclude=.vscode", "--delete", folder, full_backup_dir)
+        encrypted_file = make_encrypted_files(folder)
+        folder = "/" + folder
+        upload_file_to_cloud(dbx, folder, encrypted_file)
+
+
+def download():
+    """Download actual snapshot from Dropbox"""
+    dbx = instantiate_dropbox()
+    dbox_file = last_files_finder(dbx)
+    if not dbox_file:
+        sys.exit("File doesn\'t exist")
+    filemask = Path(dbox_file).name
+    destin_file = Path(WORK_DIR).joinpath(filemask).__str__()
+    # print(destin_file)
+
+    print("Downloading " + dbox_file + " to " + destin_file + "...")
+    # print(f'gpg -d -o {filemask[:23]} {filemask}')
+    try:
+        dbx.files_download_to_file(destin_file, dbox_file)
+    except ApiError as err:
+        print(err)
+        sys.exit()
+    os.chdir(os.path.join(WORK_DIR, 'projects'))
+    gpg("-d", "-o", filemask[:23], filemask)
+    shutil.unpack_archive(filemask[:23])
+    
+
+def instantiate_dropbox():
+    """ Make Dropbox instance"""
+    token = os.environ.get("DBOX_TOKEN", '')
+    if (len(token) == 0):
+        sys.exit("ERROR: Looks like you didn't add your access token.")
+    print("Creating a Dropbox object...")
+    dbx = dropbox.Dropbox(token)
+    
+    try:
+        dbx.users_get_current_account()
+    except AuthError as err:
+        print(err)
+        sys.exit(
+            "ERROR: Invalid access token; try re-generating \
+                an access token from the app console on the web.")
+    return dbx
+
+def upload_file_to_cloud(dbox_instance, dest_folder, dest_file):
     """Uploads content of backup files to Dropbox"""
     dbox_path = Path(dest_folder).joinpath(dest_file).__str__()
     with open(dest_file, 'rb') as f:
@@ -35,45 +89,6 @@ def upload_file(dbox_instance, dest_folder, dest_file):
                 sys.exit()
     clean_dbox_folder(dbox_instance, dest_folder)
     dbox_list_files(dbox_instance, dest_folder)
-
-
-def download_file():
-    """Download actual snapshot from Dropbox"""
-    dbx = instantiate_dropbox()
-    dbox_file = last_files_finder(dbx)
-    filemask = Path(dbox_file).name
-    destin_file = Path(WORK_DIR).joinpath(filemask).__str__()
-    # print(destin_file)
-
-    if not dbox_file:
-        sys.exit("File doesn\'t exist")
-
-    print("Downloading " + dbox_file + " to " + destin_file + "...")
-    print(f'gpg -d -o {filemask[:23]} {filemask}')
-    try:
-        dbx.files_download_to_file(destin_file, dbox_file)
-    except ApiError as err:
-        print(err)
-        sys.exit()
-    os.chdir(WORK_DIR)
-    gpg("-d", "-o", filemask[:23], filemask)
-
-def instantiate_dropbox():
-    """ Make Dropbox instance"""
-    token = os.environ.get("DBOX_TOKEN", '')
-    if (len(token) == 0):
-        sys.exit("ERROR: Looks like you didn't add your access token.")
-    print("Creating a Dropbox object...")
-    dbx = dropbox.Dropbox(token)
-    
-    try:
-        dbx.users_get_current_account()
-    except AuthError as err:
-        print(err)
-        sys.exit(
-            "ERROR: Invalid access token; try re-generating \
-                an access token from the app console on the web.")
-    return dbx
 
 def clean_dbox_folder(dbox_instance, dbox_dir, days=RETENTION_PEROD):
     """Removes all files in dbox_dir"""
@@ -104,23 +119,6 @@ def dbox_list_files(dbox_instance, dbox_dir, last=6):
     print("Files list:")
     for entry in dbox_instance.files_list_folder(dbox_dir).entries[-last:]:
         print(entry.name)
-
-def sync_entities():
-    """Copy files, folders to destination"""
-    source_list = SOURCE_ITEMS
-    dbx = instantiate_dropbox()
-    remove_old_files(WORK_DIR)
-    relative_dirs = list(source_list.keys())
-    for folder in relative_dirs:
-        full_backup_dir = Path(WORK_DIR).joinpath(folder).__str__()
-        for source_dir in source_list[folder]:
-            rsync("-avrh", "--exclude=*.pyc", "--exclude=*.log*", "--exclude=.vscode", "--delete", source_dir, full_backup_dir) # "-R"
-        # rsync("-avrh", "--exclude=*.pyc", "--exclude=*.log*", "--exclude=.vscode", "--delete", folder, full_backup_dir)
-        encrypted_file = make_encrypted_files(folder)
-        folder = "/" + folder
-        upload_file(dbx, folder, encrypted_file)
-        
-        
 
 def make_encrypted_files(path):
     """Make encrypted files"""
