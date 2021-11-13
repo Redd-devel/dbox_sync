d0a1e8de1a4a04381ea19ffed8c5e546c503620f
Redd-devel <ms_redgard@hotmail.com>
2021-11-13 20:53:50 +0600

    Removed commented functions

--- shared_libs/fs_lib.py
+++ shared_libs/fs_lib.py
@@ -3,41 +3,6 @@ import shutil
 import subprocess
 from shared_libs.dbox_config import SOURCE_ITEMS, WORK_DIR, GPG_ID, CURRENT_DATE
 
-# def delete_old_project():
-#     """Removes unactual projects from backup folder"""
-#     for item in SOURCE_ITEMS.keys():
-#         os.chdir(os.path.join(WORK_DIR, item))
-#         fs_list_dirs = os.listdir()
-#         for synced_dir in SOURCE_ITEMS[item]:
-#             try:
-#                 fs_list_dirs.remove(os.path.basename(synced_dir))
-#             except ValueError:
-#                 continue
-#         if not fs_list_dirs:
-#             return
-#         for dir_to_remove in fs_list_dirs:
-#             try:
-#                 shutil.rmtree(dir_to_remove)
-#             except OSError as err:
-#                 print(f'Cannot remove {dir_to_remove}. Error {err}')
-
-
-# def sync_local_dirs(source, dest):
-#     """Synchronizes 2 folders"""
-#     subprocess_args = [
-#         'rsync', '-avrh',
-#         '--exclude=*.pyc',
-#         '--exclude=*.log*',
-#         '--exclude=.vscode',
-#         '--delete',
-#         source,
-#         dest
-#     ]
-#     try:
-#         subprocess.check_call(subprocess_args)
-#     except subprocess.CalledProcessError as err:
-#         print('PRINT:', err)
-
 
 class LocalActions:
     """Presents tools for FS works"""   
