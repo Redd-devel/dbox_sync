0d6ed345a517bdb39e0357fa775ae1237e7bf8ac
Redd-devel <ms_redgard@hotmail.com>
2022-11-01 22:07:33 +0600

    a miserable commit 01

--- shared_libs/dbox_sync_lib.py
+++ shared_libs/dbox_sync_lib.py
@@ -8,77 +8,74 @@ import shutil
 from dotenv import dotenv_values
 
 from dropbox.exceptions import ApiError, AuthError
-from shared_libs.dbox_config import WORK_DIR, SOURCE_ITEMS, \
-                GPG_ID, CURRENT_DATE, RETENTION_PEROD
-from shared_libs.fs_lib import delete_old_project, sync_local_dirs
+from shared_libs.dbox_config import WORK_DIR, SOURCE_ITEMS, RETENTION_PEROD
+from shared_libs.fs_lib import delete_old_project, sync_local_dirs, remove_old_files, make_encrypted_files, keys_list, check_dir
 
 
 def upload():
     """Copy files, folders to destination"""
-    delete_old_project()
+    # need refactoring
     dbx = instantiate_dropbox()
-    relative_dirs = list(SOURCE_ITEMS.keys())
-    for folder in relative_dirs:
+    for folder in keys_list():
+        delete_old_project(folder)
         full_backup_dir = Path(WORK_DIR).joinpath(folder).__str__()
         for source_dir in SOURCE_ITEMS[folder]:
             sync_local_dirs(source_dir, full_backup_dir)
         encrypted_file = make_encrypted_files(folder)
-        folder = "/" + folder
-        upload_file_to_cloud(dbx, folder, encrypted_file)
+        upload_file_to_cloud(dbx, f'/{folder}', encrypted_file)
     remove_old_files(WORK_DIR)
-
 
 def download():
     """Download actual snapshot from Dropbox"""
+    # reviewed
     dbx = instantiate_dropbox()
-    dbox_file = last_files_finder(dbx)
     config = dotenv_values('.env')
-    if not dbox_file:
-        sys.exit("File doesn\'t exist")
-    filemask = Path(dbox_file).name
-    destin_file = os.path.join(WORK_DIR, 'projects', filemask)
-
-    print("Downloading " + dbox_file + " to " + destin_file + "...")
-    projects_dir = os.path.join(WORK_DIR, 'projects')
-    os.chdir(projects_dir)
-    try:
-        dbx.files_download_to_file(destin_file, dbox_file)
-    except ApiError as err:
-        print(err)
-        sys.exit()
-    try:
-        subprocess.run(
-        ['gpg', '--pinentry-mode', 'loopback', 
-        '--passphrase', config["GPG_PASS"], '--batch', '-d', '-o',
-         filemask[:23], filemask]
-    )
-    except subprocess.CalledProcessError as err:
-        print('ERROR: ', err)
-    shutil.unpack_archive(filemask[:23])
-    for destination in SOURCE_ITEMS['projects']:
-        source = os.path.join(projects_dir, os.path.basename(destination))
-        sync_local_dirs(source, os.path.dirname(destination))
-    remove_old_files(projects_dir)
+    for item in keys_list():
+        dbox_file = last_files_finder(dbx, item)
+        if not dbox_file:
+            continue
+        filemask = Path(dbox_file).name
+        destin_file = os.path.join(WORK_DIR, item, filemask)
+        print("Downloading " + dbox_file + " to " + destin_file + "...")
+        item_dir = os.path.join(WORK_DIR, item)
+        check_dir(item_dir)
+        try:
+            dbx.files_download_to_file(destin_file, dbox_file)
+        except ApiError as err:
+            print(f'ERROR: {err}')
+        try:
+            subprocess.run(
+            ['gpg', '--pinentry-mode', 'loopback', 
+            '--passphrase', config["GPG_PASS"], '--batch', '-d', '-o',
+            filemask[:23], filemask]
+        )
+        except subprocess.CalledProcessError as err:
+            print(f'ERROR: {err}')
+        shutil.unpack_archive(filemask[:23])
+        for destination in SOURCE_ITEMS[item]:
+            source = os.path.join(item_dir, os.path.basename(destination))
+            sync_local_dirs(source, os.path.dirname(destination))
+        remove_old_files(item_dir)
 
 def instantiate_dropbox():
     """ Make Dropbox instance"""
+    # reviewed
     token = os.environ.get("DBOX_TOKEN", '')
     if (len(token) == 0):
-        sys.exit("ERROR: Looks like you didn't add your access token.")
+        sys.exit("ERROR: Looks like your access token is empty.")
     print("Creating a Dropbox object...")
     dbx = dropbox.Dropbox(token)
     
     try:
         dbx.users_get_current_account()
     except AuthError as err:
-        print(err)
-        sys.exit(
-            "ERROR: Invalid access token; try re-generating \
-                an access token from the app console on the web.")
+        print("ERROR: Invalid access token; try re-generating \
+                an access token from the app console on the web.", err)
     return dbx
 
 def upload_file_to_cloud(dbox_instance, dest_folder, dest_file):
     """Uploads content of backup files to Dropbox"""
+    # reviewed, but pay attention to exception handling
     dbox_path = Path(dest_folder).joinpath(dest_file).__str__()
     with open(dest_file, 'rb') as f:
         # We use WriteMode=overwrite to make sure that the settings in the file
@@ -104,6 +101,7 @@ def upload_file_to_cloud(dbox_instance, dest_folder, dest_file):
 
 def clean_dbox_folder(dbox_instance, dbox_dir, days=RETENTION_PEROD):
     """Removes all files in dbox_dir"""
+    # reviewed
     time_diff = datetime.datetime.now() - datetime.timedelta(days=days)
     for file in dbox_instance.files_list_folder("/"+dbox_dir).entries:
             if file.server_modified < time_diff:
@@ -113,43 +111,25 @@ def clean_dbox_folder(dbox_instance, dbox_dir, days=RETENTION_PEROD):
                 except ApiError as err:
                     print(f'Something wrong with {file.path_display}. Reason: {err}')
 
-def last_files_finder(dbox_obj):
+def last_files_finder(dbox_instance, direct):
     """Find last actual file"""
+    # 
     days=0
     while days < 182:
         date_mask = (datetime.date.today() - \
             datetime.timedelta(days=days)).strftime("%Y-%m-%d")
-        filemask = "projects_" + date_mask + ".zip.asc"
-        if dbox_obj.files_search("/projects", filemask).matches:
-            return dbox_obj.files_search("/projects",
+        # for item in keys_list():
+            # filemask = item + "_" + date_mask + ".zip.asc"
+        filemask = f"{direct}_{date_mask}.zip.asc"
+        if dbox_instance.files_search(f"/{direct}", filemask).matches:
+            return dbox_instance.files_search(f"/{direct}",
                 filemask).matches[0].metadata.path_display
         days += 1
     return
 
 def dbox_list_files(dbox_instance, dbox_dir, last=6):
     """Files list of destination DropBox folder"""
-    print("Files list:")
+    # reviewed
+    print(f'Last {last} files list:')
     for entry in dbox_instance.files_list_folder(dbox_dir).entries[-last:]:
         print(entry.name)
-
-def make_encrypted_files(path):
-    """Make encrypted files"""
-    os.chdir(WORK_DIR)
-    base_file_name = path + '_' + CURRENT_DATE
-    shutil.make_archive(base_file_name, "zip", path)
-    arch_name = base_file_name + ".zip"
-    try:
-        subprocess.run(['gpg', '-ear', GPG_ID, arch_name])
-    except subprocess.CalledProcessError as err:
-        print('ERROR:', err)
-    return arch_name + ".asc"
-
-def remove_old_files(temp_dir):
-    """Removes all files in 'temp_dir' folder"""
-    with os.scandir(temp_dir) as curr_dir:
-        for item in curr_dir:
-            if item.is_file():
-                try:
-                    os.unlink(item)
-                except Exception as err:
-                    print(f'Failed to remove {item}. Reason: {err}')
--- shared_libs/fs_lib.py
+++ shared_libs/fs_lib.py
@@ -1,27 +1,25 @@
 import os
 import shutil
 import subprocess
-from shared_libs.dbox_config import SOURCE_ITEMS, WORK_DIR
+from shared_libs.dbox_config import SOURCE_ITEMS, WORK_DIR, CURRENT_DATE, GPG_ID
 
 
-def delete_old_project():
+def delete_old_project(folder):
     """Removes unactual projects from backup folder"""
-    for item in SOURCE_ITEMS.keys():
-        os.chdir(os.path.join(WORK_DIR, item))
-        fs_list_dirs = os.listdir()
-        for synced_dir in SOURCE_ITEMS[item]:
-            try:
-                fs_list_dirs.remove(os.path.basename(synced_dir))
-            except ValueError:
-                continue
-        if not fs_list_dirs:
-            return
-        for dir_to_remove in fs_list_dirs:
-            try:
-                shutil.rmtree(dir_to_remove)
-            except OSError as err:
-                print(f'Cannot remove {dir_to_remove}. Error {err}')
-
+    os.chdir(os.path.join(WORK_DIR, folder))
+    fs_list_dirs = os.listdir()
+    for synced_dir in SOURCE_ITEMS[folder]:
+        try:
+            fs_list_dirs.remove(os.path.basename(synced_dir))
+        except ValueError:
+            continue
+    if not fs_list_dirs:
+        return
+    for dir_to_remove in fs_list_dirs:
+        try:
+            shutil.rmtree(dir_to_remove)
+        except OSError as err:
+            print(f'Cannot remove {dir_to_remove}. Error {err}')
 
 def sync_local_dirs(source, dest):
     """Synchronizes 2 folders"""
@@ -30,6 +28,8 @@ def sync_local_dirs(source, dest):
         '--exclude=*.pyc',
         '--exclude=*.log*',
         '--exclude=.vscode',
+        '--exclude=.git',
+        '--exclude=dbox_config.py',
         '--delete',
         source,
         dest
@@ -39,6 +39,63 @@ def sync_local_dirs(source, dest):
     except subprocess.CalledProcessError as err:
         print('PRINT:', err)
 
+def remove_old_files(temp_dir):
+    """Removes all files in 'temp_dir' folder"""
+    # reviewed
+    with os.scandir(temp_dir) as curr_dir:
+        for item in curr_dir:
+            if item.is_file():
+                try:
+                    os.unlink(item)
+                except Exception as err:
+                    print(f'Failed to remove {item}. Reason: {err}')
+
+def make_encrypted_files(path):
+    """Make encrypted files"""
+    # reviewed
+    os.chdir(WORK_DIR)
+    base_file_name = path + '_' + CURRENT_DATE
+    try:
+        shutil.make_archive(base_file_name, "zip", path)
+    except Exception as err:
+        print('ERROR:', err)
+    arch_name = base_file_name + ".zip"
+    try:
+        subprocess.run(['gpg', '-ear', GPG_ID, arch_name])
+    except subprocess.CalledProcessError as err:
+        print('ERROR:', err)
+    return arch_name + ".asc"
+
+def check_gpg() -> bool:
+    """Chech installed gpg"""
+    # in developing
+    devnull = open(os.devnull,"w")
+    retval = subprocess.call(["dpkg","-s","gpg"],stdout=devnull,stderr=subprocess.STDOUT)
+    devnull.close()
+    if retval != 0:
+        print("Package gpg not installed.")
+        return False
+    return True
+
+def check_gpg_key() -> bool:
+    """Check gpg_id"""
+    # in developing
+    retval = subprocess.run(["gpg", "-k"], stdout=subprocess.PIPE, encoding='utf-8')
+    if GPG_ID not in retval.stdout:
+        print("There is no target key. Program aborted")
+        return False
+    return True
+
+def keys_list() -> list:
+    return list(SOURCE_ITEMS.keys())
+
+def check_dir(folder: str) -> None:
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
+        print(f'{folder} created')
+    else:
+        os.chdir(folder)
 
 if __name__ == '__main__':
-    delete_old_project()
+    # delete_old_project()
+    check_gpg_key()
