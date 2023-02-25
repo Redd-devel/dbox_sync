6b975172fa1bee1ac68ba98d0f8f93886afa7617
Redd-devel <ms_redgard@hotmail.com>
2023-02-25 11:45:32 +0600

    Argparse uses instead of sys

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import sys
+import argparse
 from shared_libs import download, upload
 
+parser = argparse.ArgumentParser(description='DropBox and Yandex clouds executor')
+parser.add_argument('action', type=str, help='Choose an action', choices=['upload', 'dowload'])
+
+args = parser.parse_args()
 
 def main():
-    if sys.argv[1] == "upload":
+    if args.action == "upload":
         upload()
-    elif sys.argv[1] == "download":
+    elif args.action == "download":
         download()
-    else:
-        print("error")
-        sys.exit(1)
 
 main()
