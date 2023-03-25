2208162a62d02e78a2b4e18cd970ac58b052c5b7
Redd-devel <ms_redgard@hotmail.com>
2023-03-25 10:18:43 +0600

    added confparser for research

---
+++ shared_libs/conf_parser.py
@@ -0,0 +1,22 @@
+import configparser
+
+
+def createConfig(path):
+    """
+    Create a config file
+    """
+    config = configparser.ConfigParser()
+    config.add_section("global_settings")
+    config.set("global_settings", "item0", "value0")
+    config.set("global_settings", "item1", "value1")
+    config.add_section("projects")
+    config.set("projects", "item0", "value0")
+    config.set("projects", "item1", "value1")
+
+    with open(path, "w") as conf_file:
+        config.write(conf_file)
+
+
+# if __name__ == "__main__":
+#     path = "settings.ini"
+#     createConfig(path)
